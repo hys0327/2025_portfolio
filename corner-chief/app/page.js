d80fcfc17d8fc68c1dbe7b56ba7b58808cb58729
hys-0327 <dbtlawleh@gmail.com>
2025-07-21 18:34:18 +0900

    레이아웃 구성(홈, 레시피)

--- corner-chief/app/page.js
+++ corner-chief/app/page.js
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      안녕하세요.
-    </div>
-  );
+    return (
+        <div>
+            <p>홈 화면 입니다</p>
+        </div>
+    );
 }
